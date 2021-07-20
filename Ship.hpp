5927c4f64e5ca6186cd0127a3616694d246e7938
BarTes8 <barbara.tessema@gmail.com>
2021-07-21 01:53:52 +0200

    added setName() in class Ship

--- Ship.hpp
+++ Ship.hpp
@@ -7,7 +7,7 @@ public:
     Ship()
         : id_(1)
     {}
-    Ship(size_t id, std::string name, size_t speed, size_t maxCrew, size_t capacity)
+    Ship(size_t id, const std::string& name, size_t speed, size_t maxCrew, size_t capacity)
         : id_(id)
         , name_(name)
         , speed_(speed)
@@ -17,6 +17,8 @@ public:
     Ship(size_t id, size_t speed, size_t maxCrew) 
         : Ship(id, "", speed, maxCrew, 0)
     {}
+
+    void setName(const std::string& name) { name_ = name; }
     
     size_t getId() const        { return id_; }
     std::string getName() const { return name_; }
@@ -25,7 +27,7 @@ public:
     size_t getCapacity() const  { return capacity_; }
 
 private:
-    size_t id_;
+    const size_t id_;
     std::string name_;
     size_t speed_;
     size_t maxCrew_;
