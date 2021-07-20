9ad6ec66a08ef4e31ce032f06fed3f8c285ef75c
BarTes8 <barbara.tessema@gmail.com>
2021-07-21 01:57:54 +0200

    added crew_ in Ship class

--- Ship.hpp
+++ Ship.hpp
@@ -23,6 +23,7 @@ public:
     size_t getId() const        { return id_; }
     std::string getName() const { return name_; }
     size_t getSpeed() const     { return speed_; }
+    size_t getCrew() const      { return crew_; }
     size_t getMaxCrew() const   { return maxCrew_; }
     size_t getCapacity() const  { return capacity_; }
 
@@ -30,6 +31,7 @@ private:
     const size_t id_;
     std::string name_;
     size_t speed_;
+    size_t crew_;
     size_t maxCrew_;
     size_t capacity_;
 };
