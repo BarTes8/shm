a306ef241b89f6f607fcdd6e686300b1650661a7
BarTes8 <barbara.tessema@gmail.com>
2021-07-21 03:37:21 +0200

    added getters to class Cargo

--- Cargo.hpp
+++ Cargo.hpp
@@ -8,6 +8,10 @@ public:
     Cargo& operator-=(size_t amount);
     bool operator==(const Cargo& cargo) const;
 
+    std::string getName() const { return name_; }
+    size_t getAmount() const    { return amount_; }
+    size_t getBasePrice() const { return basePrice_; }
+
 private:
     std::string name_;
     size_t amount_;
